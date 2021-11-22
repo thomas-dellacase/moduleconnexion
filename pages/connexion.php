5575aa8fe88cf30c8ed9e84ae4c552c5570d5f9d
thomas-dellacase <thomas.dellacase@laplateforme.io>
2021-11-22 12:11:43 +0100

    ok

--- pages/connexion.php
+++ pages/connexion.php
@@ -33,8 +33,8 @@ if(isset($_POST['submit'])){
         if($user == false) {
           //var_dump($user);
           $_SESSION['user'] = $login;
-          echo "welcome". $_SESSION['user']; 
-          //header("Location: ../index.php");
+          //echo "welcome". $_SESSION['user']; 
+          header("Location: ../index.php");
         }
         else{
           $failedlog = "failed mauvais mot de passe ou login";
