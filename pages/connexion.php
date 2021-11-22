b7e52389db6f8bd25c54723346d6d180235e2751
thomas-dellacase <thomas.dellacase@laplateforme.io>
2021-11-22 11:55:59 +0100

    la co

--- pages/connexion.php
+++ pages/connexion.php
@@ -28,13 +28,14 @@ if(isset($_POST['submit'])){
         $stmt->execute();
 
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        
         if($user == false) {
           $_SESSION['user'] = $login;
           echo "welcome". $_SESSION['user']; 
           header("Location: ../index.php");
         }
         else{
-          $failedlog = "failed mauvais mot de passe ou login";
+          $failedlog = "failed mauvais mot de passe ou login". var_dump($user);
         }
 
     }
